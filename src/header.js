52a9cd1cff7d8ab5ba6f61fb5d7fc3df4d3f40db
Aaron Kagan <43328855+aaronkagan@users.noreply.github.com>
2023-05-14 16:36:15 -0400

    Update header.js

--- src/header.js
+++ src/header.js
@@ -1,17 +1,1 @@
-const Header = ({ languages, setActiveLanguage, activeLanguage }) => {
-  const languagesArray = Object.values(languages);
-  return (
-    <header>
-      <select
-        value={languages[activeLanguage].language}
-        onChange={(e) => setActiveLanguage(e.target.value.toLowerCase())}
-      >
-        {languagesArray.map((lang) => {
-          return <option key={lang.language}>{lang.language}</option>;
-        })}
-      </select>
-    </header>
-  );
-};
 
-export default Header;
