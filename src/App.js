8f879a1fda0ea7e70896b405cde5b48f57f36f88
Aaron Kagan <43328855+aaronkagan@users.noreply.github.com>
2023-05-14 16:31:37 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./header.js";
+import Header from "./Header.js";
 import Contact from "./Contact.js";
 import languages from "./languages.json";
 
